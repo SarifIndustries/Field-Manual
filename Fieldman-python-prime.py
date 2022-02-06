7776a2cba978d079b99f0fea49cfa6b070511580
Sarif Industries <severoffdm@gmail.com>
2022-02-07 02:08:13 +0300

    Improve sections

--- Fieldman-python-prime.py
+++ Fieldman-python-prime.py
@@ -1,12 +1,14 @@
-# Sublime quick-run: CM + B
-# Sublime delete-line: CTRL+SHIFT+K
-
+        ######################################
+        #######  PYTHON FIELD MANUAL #########
+        ######################################
 
 # Only what needed
-# Python: easy quickscript, optimistically, step-by-step execution
-# Variables style: snake_case, Blocks: 4 space indentation
-# Runs and checks line-by-line (runtime errors onlu)
+# Python: easy quickscript, optimistic, step-by-step execution
+# Runs and checks line-by-line (runtime errors only)
+# Naming style: snake_case, Blocks: 4 space indentation
 
+#=========================================
+#== PRINT
 x = 10
 p = 0.45
 print("Result:", p * x) # | Result: 4.5
@@ -17,7 +19,8 @@ str2 = 'Text' # same as double-quotes
 print(str1 * 2) # | TextText
 
 
-# String format (pyth 3.6)
+#=========================================
+#== STRING FORMAT (pyth 3.6)
 name = "Bob"
 f_string = f"Hello, {name}"
 
@@ -28,46 +31,31 @@ square_meters = 4.98765
 print(f"{square_meters:.2f}")
 
 
-# Utils
+#=========================================
+#== UTIL
 id(x)       # memory related id
 a = None    # Null
 
 
-# Console Input (can use redirects of standard input)
+#=========================================
+#== INPUT (can use redirects of standard input)
 #name = input("Enter your name: ") # .lower()
 #age_input = input("Age: ")
 #age = int(age_input) # also: int(input())
 
 
-#Tuple
-t = (1, "Text", 2) # Can omit brackets
-
-
-# List
-l = ["Bob", "Bob", "Rolf", "Smith"]
-l[0] = "Something"
-l.append("Smith")
-list(t) # Convert to list
-len(l)
-sum([1, 2, 3])
-
-
-#Set
-s1 = {"Bob", "Rolf"}
-s2 = {"Bob"}
-sd = s1.difference(s2) # Set difference (minus operation)
-su = s1.union(s2)
-si = s1.intersection(s2)
-
-
-# Boolean: True // False
+#=========================================
+#== BOOLEAN (True // False)
 5 == 5   # structural equality
-s1 is s2 # referential equality
+x is x   # referential equality
 
 
 "Bob" in ["Bob", "Smith"] # True
 "B" in "Bob"
 
+
+#=========================================
+#== IF STATEMENT
 if x == 10:
     pass # No-op
 elif x != 12 and x in (1, 2, 3):
@@ -76,7 +64,8 @@ else:
     pass
 
 
-# Loop
+#=========================================
+#== LOOP
 while x > 10:
     break
     continue
@@ -85,12 +74,32 @@ for x in [1, 2, 3]:
     print(x, end=", ")
 
 
-# List comprehension
-doubled = [n * 2 for n in [1, 3, 5]]
-starts = [s for s in ["abc", "def"] if s.startswith("a")]
+#=========================================
+#== TUPLE
+t = (1, "Text", 2) # Can omit brackets
 
 
-# Dictionary
+#=========================================
+#== LIST
+l = ["Bob", "Bob", "Rolf", "Smith"]
+l[0] = "Something"
+l.append("Smith")
+list(t) # Convert to list
+len(l)
+sum([1, 2, 3])
+
+
+#=========================================
+#== SET
+s1 = {"Bob", "Rolf"}
+s2 = {"Bob"}
+sd = s1.difference(s2) # Set difference (minus operation)
+su = s1.union(s2)
+si = s1.intersection(s2)
+
+
+#=========================================
+#== DICTIONARY
 d = {"Hero": 30, "Book": 15}
 d["Sword"] = 10
 for key in d:
@@ -99,7 +108,25 @@ for key, value in d.items():
     print(f"{key}: {value}")
 
 
-# Destructuring variables (unpacking)
+#=========================================
+#== LIST COMPREHENSION
+doubled = [n * 2 for n in [1, 3, 5]]
+starts = [s for s in ["abc", "def"] if s.startswith("a")]
+
+
+#=========================================
+#== DICTIONARY COMPREHENSIONS
+users = [
+    (0, "Bob", "password"),
+    (1, "Smith", "pass1"),
+    (2, "Agent", "qwerty"),
+]
+username_mapping = {user[1]: user for user in users}
+_, name, password = username_mapping["Bob"]
+
+
+#=========================================
+#== DESTRUCTURING VARIABLES (unpacking)
 x, y = (5, 11)
 x, y = 5, 11 # Can omit brackets, still is tuple
 
@@ -109,7 +136,8 @@ head, *tail = person # Collect tail to list
 *head, tail = person
 
 
-# Subroutines
+#=========================================
+#== SUBROUTINES
 def f():
     print("Function")
 f() # Run sub
@@ -124,28 +152,21 @@ add(4, 8)        # Positional Arguments
 add(x=4, y=23)   # Named Arguments
 
 
-# Lambda
+#=========================================
+#== LAMBDA
 lam = lambda x, y: x * y
 (lambda a: print(a))(100)
 
 
-# Higher-order functions
+#=========================================
+#== HIGHER-ORDER FUNCTIONS
 sequence = [1, 3, 5, 10]
 doubled = map(lambda x: 2 * x, sequence) # returns map object
 print(list(doubled))
 
 
-# Dictionary comprehension
-users = [
-    (0, "Bob", "password"),
-    (1, "Smith", "pass1"),
-    (2, "Agent", "qwerty"),
-]
-username_mapping = {user[1]: user for user in users}
-_, name, password = username_mapping["Bob"]
-
-
-# Varargs
+#=========================================
+#== VARARGS
 def multiply(*args):
     print(args) # Is tuple
 multiply(1, 3 ,5)
@@ -154,7 +175,8 @@ def apply(*args, operator): # Varargs + named parameter
     pass
 
 
-# Kwargs key-word arguments
+#=========================================
+#== KWARGS (keyword arguments)
 def named(**kwargs): # Into dictionary
     for key, value in kwargs.items():
         print(f"{key} :: {value}")
@@ -165,16 +187,22 @@ def combined(*args, **kwargs): # positional to args, named to kwargs
     pass
 
 
-# Destructuring arguments
+#=========================================
+#== DESTRUCTURING ARGUMENTS
 seq = [1, 4]
 multiply(*seq)  # Destructure list to positional arguments
 d = {"x": 10, "y": 20}
 add(**d)        # Destructure dictionary to named arguments
 
 
-# Python OOP
 
 
+#==============================================================================
+#                            PYTHON OOP
+#==============================================================================
+
+#=========================================
+#== CLASS
 class Agent: # Class
     TYPES = ("Special", "Regular") # Class data (not instance)
 
@@ -211,7 +239,8 @@ print(agent)
 print(f"{agent!r}") # uses repr
 
 
-# Factory-methods example
+#=========================================
+#== FACTORY-METHODS EXAMPLE
 class Book:
     TYPES = ("hardcover", "paperback")
 
@@ -236,7 +265,8 @@ heavy = Book.hardcover("Harry Potter", 1500)
 light = Book.paperback("Python 101", 600)
 
 
-# Inheritance
+#=========================================
+#== INHERITANCE
 class Hero:
     def __init__(name, hp):
         self.name = name
@@ -251,7 +281,9 @@ class Mage(Hero):
     def method():
         super().method()
 
-# Composition
+
+#=========================================
+#== COMPOSItION
 class BookShelf:
     def __init__(self, *books):
         self.books = books
@@ -270,7 +302,8 @@ shelf = BookShelf(book, book2)
 print(shelf)
 
 
-# Type hinting (pyth 3.5)
+#=========================================
+#==  TYPE HINTING (pyth 3.5)
 def aver(sequence: list) -> float: # Works with IDE or linter
     return sum(sequence) / len(sequence)
 
@@ -284,7 +317,14 @@ class Hero:
 h = Hero.create_mighty(100)
 
 
-# Importing Modules
+
+
+#==============================================================================
+#                            ADDITIONAL
+#==============================================================================
+
+#=========================================
+#==  IMPORT
 print("module: ", __name__) # Global variable with module name
 def main():
     pass
@@ -304,13 +344,15 @@ print(sys.modules) # Already imported list
 print("=" * 70)
 
 
-# Relative import
+#=========================================
+#==  RELATIVE IMPORT
 # from .somemodule import divine    // From current folder
 # from ..lib import *               // From parent folder
 # Note: All this works with specific restrictions (must be some levels deep)
 
 
-# Error handling
+#=========================================
+#==  ERROR HANDLING
 def divide(dividend, divisor):
     if divisor == 0:
         raise ZeroDivisionError("Divisor cannot be 0.")
@@ -329,7 +371,8 @@ finally:
     pass
 
 
-# Custom error
+#=========================================
+#==  CUSTOM ERROR
 class ThreeLessThanTwoError(ValueError):
     pass
 if 3 < 2:
@@ -338,7 +381,8 @@ if 3 < 2:
     )
 
 
-# First-class functions
+#=========================================
+#==  FIRST-CLASS FUNCtION
 def add(a, b):
     return a + b
 
@@ -349,7 +393,8 @@ calc(3, 4, operator=add)
 calc(1, 2, 3, operator=lambda *args: sum(args))
 
 
-# Decorators
+#=========================================
+#==  DECORATORS
 user = {"username": "G1", "access_level": "guest"}
 
 def just_get_secret():
@@ -418,7 +463,8 @@ def get_dashboard_password():
     return "user: user_password"
 
 
-# Mutability
+#=========================================
+#==  MUTABILITY
 a = []
 b = a # Same object in memory
 print(id(a))
@@ -441,17 +487,6 @@ class Student:
     def __init__(self, name: str, grades: List[int] = None):
         self.name = name
         self.grades = grades or []  # New list created if one isn't passed
-
-
-
-
-
-
-
-
-
-
-
 
 
 
